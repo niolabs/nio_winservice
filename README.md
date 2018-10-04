9a8f2785b670e96677c145cce22bf85df56d50f9
Tom Young <tyoung@n.io>
2018-10-04 16:21:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Install nio as Windows Service
+
+### Requirements:
+- [pywin32](https://pypi.org/project/pywin32/) (should have been installed with nio)
+- `nio_winservice.py` from this repo
+- A nio project
+
+### Install:
+By default Windows will install the new service to be run by the `Local System` account, which requires that Python and niod be in the path for that account. Launch `regedit` and navigate to `Computer > HKEY_LOCAL_MACHINE > SYSTEM > CurrentControlSet > Control > Session Manager > Environment`. Modify `Path` and, using semicolons as seperators, add the aboslute path to Python and niod directories at the end. For help finding these paths, run `where python` and `where niod`, truncating the `exe` name from the returned path.
+- Open `nio_winservice.py` with a text editor and enter the abosulte path to your nio project for the value of `project_path` near the top of the `NIOWinService` class, for example `project_path = C:\\Users\\<user>\\nio\\projects\\<project>` **Note the double backslashes!**
+- Run `python nio_winservice --startup auto|manual install`
+- `nio_<project>` has been installed and will start automatically in the future.
+
+### Use:
+- Control the service manually from the command line with `net start|stop nio_<project>`
+- Or, Open Control Panel > System and Security > Adminstrative Tools > Services
+
+### Remove:
+- Run `python nio_winservice remove`
---
+++ nio_winservice.py
@@ -0,0 +1,80 @@
+"""
+
+    NIO Windows Service
+
+"""
+from signal import CTRL_BREAK_EVENT
+import subprocess
+import win32service
+import win32event
+import sys
+import win32api
+import win32console
+import win32serviceutil
+import socket
+
+
+class NIOWinService(win32serviceutil.ServiceFramework):
+
+    project_path = 'C:\\Users\\Tom\\nio\\projects\\bacnet'
+    name = 'nio_{}'.format(project_path.split('\\')[-1])
+    _svc_display_name_ = name
+    _svc_name_ = name
+    _svc_description_ = project_path
+
+    def __init__(self, *args):
+        win32serviceutil.ServiceFramework.__init__(self, *args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+        self.process = None
+
+    def log(self, msg):
+        import servicemanager
+        servicemanager.LogInfoMsg(str(msg))
+
+    def error(self, msg):
+        import servicemanager
+        servicemanager.LogErrorMsg(str(msg))
+
+    def SvcDoRun(self):
+        self.log('Triggered ...')
+        self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
+        try:
+            self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+            self.log('Starting ...')
+            self.start()
+            win32event.WaitForSingleObject(self.hWaitStop,
+                                           win32event.INFINITE)
+            self.log('Exit')
+        except Exception as x:
+            self.error('Exception : %s' % str(x))
+            self.SvcStop()
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        self.log('Stopping ....')
+        self.stop()
+        self.log('Stopped')
+        win32event.SetEvent(self.hWaitStop)
+        self.ReportServiceStatus(win32service.SERVICE_STOPPED)
+
+    def start(self):
+        self.log('Launching nio Daemon')
+        win32console.AllocConsole()
+        self.process = subprocess.Popen(
+            ['niod'],
+            cwd=self.project_path,
+            creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+
+    def stop(self):
+        try:
+            self.log('Stopping nio Daemon')
+            if self.process:
+                win32api.GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT,
+                                                  self.process.pid)
+                self.log('Signal sent to stop nio')
+        except Exception as x:
+            self.error('Exception stopping: %s' % str(x))
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(NIOWinService)
